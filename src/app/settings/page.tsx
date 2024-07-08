e5c9f764d0ee962188b97c29f69d54197d1f0d17
shixin.guo <shixin.guo@zoom.us>
2024-07-08 15:57:28 -0700

    fix:bug

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -22,7 +22,7 @@ import {
 import { Input } from "@/components/ui/input"
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 
-export function SettingPage() {
+export default function SettingPage() {
   return (
     <div className="flex min-h-screen w-full flex-col">
       <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
